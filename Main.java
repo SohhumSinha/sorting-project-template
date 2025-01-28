2b626fd4736e37cf0249d134792acc6372db4b03
SohhumSinha <sohhumcs@gmail.com>
2025-01-28 04:40:15 +0000

    clean up

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-       int[] testInput = generateRandomArray(10);  // Change the size of the array as needed
+       int[] testInput = generateRandomArray(10); 
 
         System.out.println("Original array:");
         printArray(testInput);
 
-        TestSuite.run(testInput, 10000);  // You can adjust the number of loops as necessary
+        TestSuite.run(testInput, 10000);  
     }
 
     public static int[] generateRandomArray(int size) {
         Random random = new Random();
         int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            array[i] = random.nextInt(100);  // Generates numbers between 0 and 99
+            array[i] = random.nextInt(100);  
         }
         return array;
     }
